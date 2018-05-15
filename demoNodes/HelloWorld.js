0af994ee0d971ab9acb607dc9ae69f67ded99ba1
Vignesh Ramachandra <vickyramachandra@gmail.com>
2018-05-15 11:25:56 +0530

    Setting up custom node done

---
+++ demoNodes/HelloWorld.js
@@ -0,0 +1,14 @@
+module.exports = function(RED) {
+    function helloWorld(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        this.on('input', function(msg) {
+
+        var outMsg = {payload: "Hello World"};
+
+        node.send(outMsg);
+
+        });
+    }
+    RED.nodes.registerType("Hello World",helloWorld);
+};
